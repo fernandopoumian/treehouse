9b3001dbb0b9a60ee94b9eb20a5374c3b1beafbe
Fernando Poumián <fpoumian@postmm.com>
2016-06-04 15:37:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var router = require("./router.js");
+
+//Problem: We need a simple way to look at a user's badge count and JavaScript points from a web browser
+//Solition: Use Node.js to perform the profile look ups and serve our template via HTTP
+
+
+//1. Create a web server
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+
+    router.home(request, response);
+    router.user(request, response);
+    //response.end();
+
+    //response.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
